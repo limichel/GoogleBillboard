b6881f126f0284fdbf491b0ef9f3f3311b27cbc1
unknown <mili7@697-334-Smaug.sf.k12.internal>
2014-12-09 11:31:27 -0800

    Edited to find first 10-digit prime number in e.

--- build-tmp/source/GoogleBillboard.java
+++ build-tmp/source/GoogleBillboard.java
@@ -21,11 +21,27 @@ public void setup()
 }  
 public void draw()  
 {   
-	//not needed for this assignment
+	for(int pos = 2; pos < e.length()-9; pos++)
+    {
+    	String digits = e.substring(pos, pos+10);
+    	double dNum = Double.parseDouble(digits);
+    	if(isPrime(dNum) == true)
+    	{
+    		System.out.println(dNum);
+    		break;
+    	}
+    }
+	
 }  
 public boolean isPrime(double dNum)  
 {   
-    //to be finished later   
+    for(int i = 2; i < sqrt((float)dNum); i++)
+    {
+    	if(dNum % i == 0)
+    	{
+    		return false;
+    	}
+    }
     return true;  
 } 
   static public void main(String[] passedArgs) {
